9f1788c54c43981040b606450134f3b9f8d7560a
Numair <Numair2644@protonmail.com>
2023-12-10 13:24:22 -0500

    Revert to amixer cmds for volume controls (cleaner, more compatible, and faster)

--- home/.config/i3blocks.symlink/bin/volume.sh
+++ home/.config/i3blocks.symlink/bin/volume.sh
@@ -1,19 +1,32 @@
 #!/bin/sh
 
+low="󰕿 "
+hi="󰕾 "
+med="󰖀 "
+mute="󰝟"
+
+# Mouse actions
 case $BLOCK_BUTTON in
-    1) wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle ;;
-    4) wpctl set-volume @DEFAULT_AUDIO_SINK@ "5%+" ;;
-    5) wpctl set-volume @DEFAULT_AUDIO_SINK@ "5%-" ;;
+	1) amixer --quiet sset Master toggle ;;
+	4) amixer --quiet set Master 5%+  ;;
+	5) amixer --quiet set Master 5%-  ;;
 esac
 
-vol_l="󰕿 󰖀 󰕾 󰕾 󰕾 "
-vol_n="󰝟 "
+# TODO: this assumes left and right volume is the same
+volume=$(amixer sget Master | grep -o '1\?[0-9]\?[0-9]%' | head -n1 | tr -d '%\n')
 
-vol="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | sed -e 's/\.//' -e 's/^Volume: //' -e 's/^0\?0\?//')"
-vol_lvl="$(printf "%s 25 / 1 + p" "$vol" | dc)"
+get_vol_icon() {
+	if [ "$volume" -gt 50 ]; then
+		echo " $hi$volume "
+	elif [ "$volume" -gt 25 ]; then
+		echo " $med$volume "
+	else
+		echo " $low$volume "
+	fi
+}
 
-if wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -q '\[MUTED\]'; then
-    echo " $vol_n"
+if amixer get Master | grep -q '\[on\]'; then
+	get_vol_icon
 else
-    printf " %s %s \n" "$(echo "$vol_l" | cut -d' ' -f"$vol_lvl")" "$vol"
+	echo "$mute"
 fi
