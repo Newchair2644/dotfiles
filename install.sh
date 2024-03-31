e361bf166017a363570b5b5d03e0ef4d1d4115eb
Newchair2644 <Numair2644@protonmail.com>
2024-03-31 02:09:23 -0400

    Ability to call specifiic function rather than all actions base install config option Updated groups, services, packages, and fonts

--- install.sh
+++ install.sh
@@ -2,11 +2,15 @@
 # Install script for my entire system
 # Config backups are made automatically in the same dir
 
-DOTFILES_ROOT="${HOME}/.dotfiles"
-HOME_BACKUP="${HOME}/home.bak"
+# ===== Config ===== #
+DOTFILES_ROOT="${HOME}/.dotfiles"   # if you choose to move dotfiles repo elsewhere
+HOME_BACKUP="${HOME}/home.bak"      # where to backup existing ~
+INSTALL_TYPE="full"                 # choose 'full' for full install, 'base' for cli only
 
+
+# ===== Helper Functions ===== #
 # Generic yes/no prompt
-prompt() {
+_prompt() {
     printf "\r%s [\033[0;33my/N\033[0m] " "$1"
     read -r input
     case $input in
@@ -16,7 +20,7 @@ prompt() {
 }
 
 # Add "tags" to output based on $1
-tag() {
+_tag() {
     status=$1
     shift
     case $status in
@@ -34,11 +38,13 @@ tag() {
     done
 }
 
+
+# ===== "Core Actions" ===== #
 # Initial setup (create dirs, backup if needed)
 setup() {
-    echo "Backing ~/ to ${HOME_BACKUP} and creating needed dirs..." | tag 0
+    echo "Backing ~/ to ${HOME_BACKUP} and creating needed dirs..." | _tag 0
 
-    mkdir -p "$HOME_BACKUP" | tag $?
+    mkdir -p "$HOME_BACKUP" | _tag $?
     find "$HOME" -mindepth 1 -maxdepth 1 -name '.*' | grep -v "$DOTFILES_ROOT" | grep -v "$HOME_BACKUP" | xargs -I{} mv -v {} "$HOME_BACKUP"
 
     mkdir -pv "${HOME}/.config" \
@@ -47,60 +53,83 @@ setup() {
         "${HOME}/.local/share/fonts"
 }
 
-# Add user to needed groups and start services
-add_groups_services() {
-    echo "Adding user to groups..." | tag 0
-    sudo usermod -aG _seatd socklog
-    echo "Enabling services..." | tag 0
-    for service in _seatd acpid chronyd dbus dhcpcd nanoklogd pcscd polkitd power-profiles-daemon socklog-unix tlp; do
-        sudo ln -sf "/etc/sv/${service}" "/var/service/"
-        sv start "$service"
-    done
-}
-
 # Install packages with xbps-install and download some fonts
 pkg_install() {
-    echo "Installing packages..." | tag 0
-    sudo xbps-install -S $(cat pkg-list.txt)
+    echo "Installing packages..." | _tag 0
+    if [ "$INSTALL_TYPE" = "full" ]; then
+        pkg=$(sed 's/^!//' pkg-list.txt)
+    else
+        pkg=$(sed -n 's/^!//p' pkg-list.txt)
+    fi
+    sudo xbps-install -S $pkg
 
     # My favorite fonts, not all are needed, edit fonts var (see fontconfig)
-    echo "Installing fonts..." | tag 0
+    echo "Installing fonts..." | _tag 0
 
     font_src="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts"
     font_dir="${HOME}/.local/share/fonts"
     fonts="\
+        FiraCode/Regular/FiraCodeNerdFont-Regular.ttf
         JetBrainsMono/Ligatures/Regular/JetBrainsMonoNerdFont-Regular.ttf
+        Mononoki/Regular/MononokiNerdFont-Regular.ttf
         Iosevka/Regular/IosevkaNerdFont-Regular.ttf
-        Ubuntu/Regular/UbuntuNerdFont-Regular.ttf
-        Mononoki/Regular/MononokiNerdFont-Regular.ttf"
+        Ubuntu/Regular/UbuntuNerdFont-Regular.ttf"
 
     for font in $fonts; do
         curl -OJL --output-dir "$font_dir" "${font_src}/${font}"
     done
 }
 
-# Symlink dotfiles, create backup if $dst already exists, then wallpapers
-dotfiles_install() {
-    for src in $(find "$DOTFILES_ROOT" -name '*.symlink' -not -path '*.git*'); do
-        dst="$(echo "$src" | sed -e "s/\.symlink$//g" -e "s|${DOTFILES_ROOT}\/home|$HOME|")"
-        if ! [ "$src" -ef "$(readlink "$dst")" ]; then
-            if [ -f "$dst" ] || [ -d "$dst" ]; then
-                backup="$dst-$(date +%Y-%m-%d)"
-                mv --verbose --backup "$dst" "${backup}.backup" | tag $?
-            fi
-            ln --verbose --symbolic "$src" "$dst" | tag $?
-    	fi
+# Add user to needed groups and start services TODO packages (before groups, also root access???)
+add_groups_services() {
+    echo "Adding user to groups..." | _tag 0
+    sudo usermod -aG dialout audio video kvm input users _seatd socklog bluetooth lpadmin
+    echo "Enabling services..." | _tag 0
+    for service in acpid dbus seatd chronyd pcscd nanoklogd socklog-unix adb tlp cupsd udevd popcorn iwd; do
+        sudo ln -sf "/etc/sv/${service}" "/var/service/"
+        sv start "$service"
     done
 }
 
-# Flow
-main() {
+# Symlink dotfiles, create backup if $dst already exists, then wallpapers
+dotfiles_symlink() {
+    for src in $(find "$DOTFILES_ROOT" -name '*.symlink' -o -name '*.base.symlink' -not -path '*.git*'); do
+        if [ "$INSTALL_TYPE" = "base" ] && ! echo "$src" | grep -q "\.base.symlink$"; then
+            continue
+        fi
+        
+        dst="$(echo "$src" | sed -Ee "s/(\.base)?\.symlink$//g" -e "s|${DOTFILES_ROOT}/home|$HOME|")"
+        if ! [ "$src" -ef "$(readlink "$dst")" ]; then
+            if [ -f "$dst" ] || [ -d "$dst" ]; then
+                backup="$dst-$(date +%Y-%m-%d)"
+                mv --verbose --backup "$dst" "${backup}.backup" | _tag $?
+            fi
+            ln --verbose --symbolic "$src" "$dst" | _tag $?
+        fi
+    done
+}
+
+
+# ===== Flow ===== #
+_main() {
     printf "My void linux setup script.\nInstalls needed dotfiles, fonts, and packages on a fresh install.\n\n"
-    prompt "Initial setup? (creates needed directores and makes a full backup of ~)" && setup
-    prompt "Install needed packages and fonts? (needs root access)" && pkg_install
-    prompt "Add user to groups and enable services?" && add_groups_services
-    prompt "Symlink dotfiles? (will backup duplicate configs automatically)" && dotfiles_install
+    _prompt "Initial setup? (creates needed directores and makes a full backup of ~)" && setup
+    _prompt "Install needed packages and fonts? (needs root access)" && pkg_install
+    [ "$INSTALL_TYPE" = "full" ] && _prompt "Add user to groups and enable services?" && add_groups_services
+    _prompt "Symlink dotfiles? (will backup duplicate configs automatically)" && dotfiles_symlink
     echo "Setup completed. Logout and log back in tty1."
 }
 
-main
+# Allow optionally running specified action only
+if [ -n "$1" ]; then
+    case $1 in
+        setup) setup ;;
+        add-groups-services) add_groups_services ;;
+        pkg-install) pkg_install ;;
+        dotfiles-symlink) dotfiles_symlink ;;
+        dotfiles-unlinked) dotfiles_unlinked ;;
+        *) echo "Available options: setup, pkg-install, add-groups-services, dotfiles-symlink" ;;
+    esac
+else
+    _main
+fi
